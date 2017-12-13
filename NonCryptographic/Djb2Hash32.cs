ee191c0a076d1a0e1820158f7d4cf5b184b574b3
Larry Lopez <namralkeeg@gmail.com>
2017-12-12 22:51:36 -0800

    Added Djb2, and Elf hashes.

---
+++ NonCryptographic/Djb2Hash32.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Keeg.Hashing.NonCryptographic
+{
+    // Algorithm produced by Professor Daniel J.Bernstein
+    public sealed class Djb2Hash32 : HashAlgorithm
+    {
+        private static readonly uint DefaultSeed = 5381u;
+        private uint seed;
+        private uint hash;
+
+        public override int HashSize => 32;
+
+        public Djb2Hash32()
+        {
+            seed = DefaultSeed;
+            Initialize();
+        }
+
+        public Djb2Hash32(uint seed)
+        {
+            this.seed = seed;
+            Initialize();
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            for (var i = ibStart; i < ibStart + cbSize; i++)
+            {
+                hash = ((hash << 5) + hash) + array[i]; /* hash * 33 + c */
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return BitConverter.GetBytes(hash);
+        }
+    }
+}
---
+++ NonCryptographic/ElfHash32.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Keeg.Hashing.NonCryptographic
+{
+    // Similar to the PJW Hash function, widley used on UNIX based systems.
+    public sealed class ElfHash32 : HashAlgorithm
+    {
+        public override int HashSize => 32;
+        private uint hash;
+
+        public ElfHash32()
+        {
+            Initialize();
+        }
+
+        public override void Initialize()
+        {
+            hash = 0;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            uint high = 0u;
+            for (var i = ibStart; i < ibStart + cbSize; i++)
+            {
+                hash = (hash << 4) + array[i];
+                high = hash & 0xF0000000u;
+                if (high != 0)
+                {
+                    hash ^= high >> 24;
+                }
+
+                hash &= ~high;
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return BitConverter.GetBytes(hash);
+        }
+    }
+}
