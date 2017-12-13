4ba0c2ed9cb26bd9b396bd4380ec12516bef89c2
Larry Lopez <namralkeeg@gmail.com>
2017-12-12 21:54:21 -0800

    Added Crc32 and Crc64 algorithms.

---
+++ Crc/Crc64.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Keeg.Hashing.Crc
+{
+    public sealed class Crc64 : HashAlgorithm
+    {
+        public override int HashSize => 64;
+        public const ulong DefaultPolynomial = 0xD800000000000000ul; //Iso 3309 Polynomial
+        public const ulong DefaultSeed = 0x0ul;
+
+        private static ulong[] defaultTable;
+        private readonly ulong[] table;
+
+        private readonly ulong seed;
+        private ulong hash;
+
+        public Crc64()
+        {
+            seed = DefaultSeed;
+            table = InitializeTable(DefaultPolynomial);
+            Initialize();
+        }
+
+        public Crc64(ulong polynomial, ulong seed)
+        {
+            this.seed = seed;
+            table = InitializeTable(polynomial);
+            Initialize();
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            for (var i = ibStart; i < ibStart + cbSize; i++)
+            {
+                hash = (hash >> 8) ^ table[array[i] ^ (hash & 0xFF)];
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return BitConverter.GetBytes(hash);
+        }
+
+        private ulong[] InitializeTable(ulong polynomial)
+        {
+            if ((polynomial == DefaultPolynomial) && (defaultTable != null))
+                return defaultTable;
+
+            var createTable = new ulong[256];
+            for (ulong i = 0; i < 256; i++)
+            {
+                ulong entry = i;
+                for (ulong j = 0; j < 8; ++j)
+                {
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ polynomial;
+                    else
+                        entry >>= 1;
+                }
+                createTable[i] = entry;
+            }
+
+            if (polynomial == DefaultPolynomial)
+                defaultTable = createTable;
+
+            return createTable;
+        }
+    }
+}
---
+++ Crc/StringHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Keeg.Hashing.Crc
+{
+    public static partial class StringHelpers
+    {
+        internal static readonly Encoding DefaultEncoding = Encoding.UTF8;
+
+        public static uint HashCrc32(this string input, Encoding encoding)
+        {
+            var enc = encoding ?? DefaultEncoding;
+            using (HashAlgorithm hasher = new Crc32())
+            {
+                return BitConverter.ToUInt32(hasher.ComputeHash(enc.GetBytes(input)), 0);
+            }
+        }
+
+        public static uint HashCrc32(this string input)
+        {
+            return input.HashCrc32(DefaultEncoding);
+        }
+
+        public static ulong HashCrc64(this string input, Encoding encoding)
+        {
+            var enc = encoding ?? DefaultEncoding;
+            using (HashAlgorithm hasher = new Crc64())
+            {
+                return BitConverter.ToUInt64(hasher.ComputeHash(enc.GetBytes(input)), 0);
+            }
+        }
+
+        public static ulong HashCrc64(this string input)
+        {
+            return input.HashCrc32(DefaultEncoding);
+        }
+    }
+}
